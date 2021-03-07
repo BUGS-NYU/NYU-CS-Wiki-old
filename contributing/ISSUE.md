7da2046e0c9a7278c48ec08e0c5229d3cea37726
Khanh Nguyen <khanhnguyen99hn@gmail.com>
2021-03-06 20:25:40 -0500

    Add searchbar and contributing docs

---
+++ contributing/ISSUE.md
@@ -0,0 +1,22 @@
+---
+name: Bug Report
+about: Create a report to help us improve the site
+labels: bug, help wanted
+---
+
+<!-- Short summary of the bug -->
+
+**To Reproduce**
+Steps to reproduce the behavior:
+
+1. Go to '...'
+2. Click on '....'
+3. Scroll down to '....'
+4. See error
+
+**Expected behavior**
+
+<!-- What should have happened? -->
+
+**Screenshots**
+If applicable, add screenshots to help explain your problem.
---
+++ src/components/SearhBar.js
@@ -0,0 +1,44 @@
+import * as React from "react";
+import styled from "styled-components";
+
+import searchIcon from "../images/svg/search.svg";
+
+const SearchBar = () => {
+    const [query, setQuery] = React.useState("");
+
+    const onChange = (event) => {
+        console.log(event.target.value);
+        setQuery(event.target.value);
+    }
+    return <SearchContainer>
+        <SearchIcon />
+        <Bar type="test" placeholder="Search for courses or content" onChange={onChange} />
+    </SearchContainer>
+}
+
+const SearchContainer = styled.div`
+    display: flex;
+    width: 80%;
+`
+
+const Bar = styled.input`
+    width: 100%;
+    font-family: Roboto;
+    font-size: 1rem;
+    border: 0px;
+    padding: 0.6rem 0.6rem 0.6rem 2.25rem;
+
+    &:focus {
+        outline: none;
+    }
+`
+
+const SearchIcon = styled(searchIcon)`
+    position: relative;
+    width: 1.25rem;
+    height: 1.25rem;
+    margin-top: 0.5rem;
+    right: -1.5rem;
+`
+
+export default SearchBar;
