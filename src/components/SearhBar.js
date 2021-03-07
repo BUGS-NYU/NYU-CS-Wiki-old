5faaae012ea68b35bfa087d373d534f07cc4b36b
Khanh Nguyen <khanhnguyen99hn@gmail.com>
2021-03-06 20:26:27 -0500

    Format SearchBar

--- src/components/SearhBar.js
+++ src/components/SearhBar.js
@@ -4,41 +4,47 @@ import styled from "styled-components";
 import searchIcon from "../images/svg/search.svg";
 
 const SearchBar = () => {
-    const [query, setQuery] = React.useState("");
+  const [query, setQuery] = React.useState("");
 
-    const onChange = (event) => {
-        console.log(event.target.value);
-        setQuery(event.target.value);
-    }
-    return <SearchContainer>
-        <SearchIcon />
-        <Bar type="test" placeholder="Search for courses or content" onChange={onChange} />
+  const onChange = event => {
+    console.log(event.target.value);
+    setQuery(event.target.value);
+  };
+  return (
+    <SearchContainer>
+      <SearchIcon />
+      <Bar
+        type="test"
+        placeholder="Search for courses or content"
+        onChange={onChange}
+      />
     </SearchContainer>
-}
+  );
+};
 
 const SearchContainer = styled.div`
-    display: flex;
-    width: 80%;
-`
+  display: flex;
+  width: 80%;
+`;
 
 const Bar = styled.input`
-    width: 100%;
-    font-family: Roboto;
-    font-size: 1rem;
-    border: 0px;
-    padding: 0.6rem 0.6rem 0.6rem 2.25rem;
+  width: 100%;
+  font-family: Roboto;
+  font-size: 1rem;
+  border: 0px;
+  padding: 0.6rem 0.6rem 0.6rem 2.25rem;
 
-    &:focus {
-        outline: none;
-    }
-`
+  &:focus {
+    outline: none;
+  }
+`;
 
 const SearchIcon = styled(searchIcon)`
-    position: relative;
-    width: 1.25rem;
-    height: 1.25rem;
-    margin-top: 0.5rem;
-    right: -1.5rem;
-`
+  position: relative;
+  width: 1.25rem;
+  height: 1.25rem;
+  margin-top: 0.5rem;
+  right: -1.5rem;
+`;
 
 export default SearchBar;
