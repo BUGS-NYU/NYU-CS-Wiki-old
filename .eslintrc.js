d0fc165416398e718a3b65f2d0040403b84e61bc
yuchen <yl6448@nyu.edu>
2021-03-10 21:05:16 -0800

    Update config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,11 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+  ],
   globals: {
     graphql: true,
     Atomics: "readonly",
