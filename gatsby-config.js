9134a322e1482bf52018a624aee23694748bdaf9
Khanh Nguyen <khanhnguyen99hn@gmail.com>
2021-04-02 10:10:34 -0400

    Update

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,7 +79,7 @@ module.exports = {
       resolve: `gatsby-plugin-algolia`,
       options: {
         appId: process.env.GATSBY_ALGOLIA_APP_ID,
-        apiKey: process.env.GATSBY_ALGOLIA_ADMIN_KEY,
+        apiKey: process.env.ALGOLIA_ADMIN_KEY,
         queries: require("./src/utils/algolia-queries"),
       },
     },
